838f5ebe6131b868990ae9e71fd43cc4b540fac1
Matt Lashinsky <malashinsky@gmail.com>
2015-11-16 20:44:59 +0000

    added validations and create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,21 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = 'Contact info submitted, thanks hoe!'
+            redirect_to new_contact_path
+        else
+            flash[:danger] = 'We got problems, man!'
+            redirect_to new_contact_path
+        end
+        
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name,:email,:comments)
+        end
+    
 end
